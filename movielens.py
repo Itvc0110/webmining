e36a44698ab1e20bff254c5eb36e55710e1386ca
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-12-25 16:39:22 +0700

    Refactor MovieLens20MDataset for user/item mapping

--- movielens.py
+++ movielens.py
@@ -2,15 +2,24 @@ import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 
-
 class MovieLens20MDataset(Dataset):
     def __init__(self, dataset_path, sep=',', engine='c', header='infer'):
         data = pd.read_csv(dataset_path, sep=sep, engine=engine, header=header).to_numpy()[:, :3]
-        self.items = data[:, :2].astype(np.int) - 1  # -1 because ID begins from 1
+        raw_users = data[:, 0].astype(np.int64)
+        raw_items = data[:, 1].astype(np.int64)
+        
+        # Safely remap to contiguous 0-based indices
+        unique_users = np.unique(raw_users)
+        unique_items = np.unique(raw_items)
+        user_map = {uid: idx for idx, uid in enumerate(unique_users)}
+        item_map = {iid: idx for idx, iid in enumerate(unique_items)}
+        
+        self.items = np.stack([np.vectorize(user_map.get)(raw_users),
+                               np.vectorize(item_map.get)(raw_items)], axis=1)
         self.targets = self.__preprocess_target(data[:, 2]).astype(np.float32)
-        self.field_dims = np.max(self.items, axis=0) + 1
-        self.user_field_idx = np.array((0,), dtype=np.long)
-        self.item_field_idx = np.array((1,), dtype=np.long)
+        self.field_dims = np.array([len(unique_users), len(unique_items)], dtype=np.int64)
+        self.user_field_idx = np.array((0,), dtype=np.int64)
+        self.item_field_idx = np.array((1,), dtype=np.int64)
 
     def __len__(self):
         return self.targets.shape[0]
@@ -27,17 +36,13 @@ class MovieLens20MDataset(Dataset):
         unique, counts = np.unique(self.targets, return_counts=True)
         return dict(zip(unique, counts))
 
-    def user_ids(self): return np.unique(self.user_id_column())
-
-    def movie_ids(self): return np.unique(self.movie_id_column())
-
+    def user_ids(self): return np.unique(self.items[:, 0])
+    def movie_ids(self): return np.unique(self.items[:, 1])
     def user_id_column(self): return self.items[:, 0]
-
     def movie_id_column(self): return self.items[:, 1]
 
     @property
     def shape(self): return self.items.shape
-
 
 class MovieLens1MDataset(MovieLens20MDataset):
     def __init__(self, dataset_path):
