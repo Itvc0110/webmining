22d3b34d32ea0a840a41b2c56c7dfa747febb2f4
Itvc0110 <162940999+Itvc0110@users.noreply.github.com>
2025-12-28 20:53:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -43,13 +43,14 @@ def get_args():
     parser.add_argument('--epochs', type=int, default=200, help='Number of training epochs')
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--weight_decay', type=float, default=1e-4, help='Weight decay for optimizer')
-    parser.add_argument('--early_stop_patience', type=int, default=8, help='Patience for early stopping')
+    parser.add_argument('--early_stop_patience', type=int, default=20, help='Patience for early stopping')
     
     # Evaluation
     parser.add_argument('--output_dir', type=str, default='./results', help='Directory to save evaluation results and visualizations')
-    parser.add_argument('--topk', type=int, default=5, help='K for recall@K and precision@K')
+    parser.add_argument('--topk', type=int, default=10, help='K for recall@K and precision@K')
     
     return parser.parse_args()
+
 
 
 
